96e1330f979df4761bbcc0fdecbf3c05f621806e
yusuf06061995 <mohammad.yusuf@atmstech.in>
2025-08-28 16:46:48 +0530

    added manifest.json

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js",
+        "/icons/icon-192.png",
+        "/icons/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
