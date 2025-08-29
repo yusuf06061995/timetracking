717854ed948d1a4b55a13fdf71eeda5b4d505a93
yusuf06061995 <mohammad.yusuf@atmstech.in>
2025-08-29 16:02:52 +0530

    added mainfest change

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,41 @@
+const CACHE_NAME = "v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install event: pre-cache files
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("v1").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/app.js",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png"
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
+// Activate event: clean up old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch event: serve from cache, fallback to network
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return (
+        response ||
+        fetch(event.request).catch(() =>
+          caches.match("/index.html") // offline fallback
+        )
+      );
     })
   );
 });
